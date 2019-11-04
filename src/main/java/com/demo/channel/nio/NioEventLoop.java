9ee8ebc9f99b67bb71c271c704050e733e6b9d02
亚克(蒋飞) <yake.jiangf@goparcel.cn>
2019-11-04 08:34:38 +0800

    Select 策略 Channel

--- src/main/java/com/demo/channel/nio/NioEventLoop.java
+++ src/main/java/com/demo/channel/nio/NioEventLoop.java
@@ -1,9 +1,81 @@
 package com.demo.channel.nio;
 
+import com.demo.channel.SelectStrategy;
 import com.demo.channel.SingleThreadEventLoop;
+import com.demo.util.IntSupplier;
+import com.demo.util.internal.SystemPropertyUtil;
+
+import java.nio.channels.Selector;
+import java.nio.channels.spi.SelectorProvider;
+import java.security.AccessController;
+import java.security.PrivilegedAction;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * Created by jiangfei on 2019/11/2.
  */
 public final class NioEventLoop extends SingleThreadEventLoop {
+
+    private static final int CLEANUP_INTERVAL = 256;
+
+    private static final boolean DISABLE_KEY_SET_OPTIMIZATION =
+            SystemPropertyUtil.getBoolean("io.netty.noKeySetOptimization", false);
+
+    private static final int MIN_PREMATURE_SELECTOR_RETURNS = 3;
+    private static final int SELECTOR_AUTO_REBUILD_THRESHOLD;
+
+
+    private final IntSupplier selectNowSupplier = new IntSupplier() {
+        @Override
+        public int get() throws Exception {
+            return selectNow();
+        }
+    };
+
+    static {
+        final String key = "sun.nio.ch.bugLevel";
+        final String bugLevel = SystemPropertyUtil.get(key);
+        if (bugLevel == null) {
+            try {
+                AccessController.doPrivileged(new PrivilegedAction<Void>() {
+                    @Override
+                    public Void run() {
+                        System.setProperty(key, "");
+                        return null;
+                    }
+                });
+            } catch (final SecurityException e) {
+                //logger.debug("Unable to get/set System Property: " + key, e);
+            }
+        }
+
+        int selectorAutoRebuildThreshold = SystemPropertyUtil.getInt("io.netty.selectorAutoRebuildThreshold", 512);
+        if (selectorAutoRebuildThreshold < MIN_PREMATURE_SELECTOR_RETURNS) {
+            selectorAutoRebuildThreshold = 0;
+        }
+
+        SELECTOR_AUTO_REBUILD_THRESHOLD = selectorAutoRebuildThreshold;
+
+    }
+
+    //select
+    private Selector selector;
+    //unwrapped select
+    private Selector unwrappedSelector;
+    //select key set
+    private SelectedSelectionKeySet selectedKeys;
+
+    //select 提供者
+    private final SelectorProvider provider;
+
+    private final AtomicBoolean wakenUp = new AtomicBoolean();
+    private volatile long nextWakeupTime = Long.MAX_VALUE;
+
+    //选择策略
+    private final SelectStrategy selectStrategy;
+
+    private volatile int ioRatio = 50;
+    private int cancelledKeys;
+    private boolean needsToSelectAgain;
+
 }
