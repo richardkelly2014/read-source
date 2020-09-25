aa52ce50d9c5a05776f92948b5e69b60e275b543
亚克(蒋飞) <jiangfei@olayc.cn>
2020-09-25 19:08:05 +0800

    Netty 线程执行器

--- src/test/java/com/demo/rasp/ConsoleTest.java
+++ src/test/java/com/demo/rasp/ConsoleTest.java
@@ -1,8 +1,9 @@
 package com.demo.rasp;
 
-import com.pi4jl.util.Console;
-import com.pi4jl.util.ConsoleColor;
+
 import org.junit.Test;
+
+import java.io.Console;
 
 /**
  * Created by jiangfei on 2019/11/16.
@@ -11,8 +12,7 @@ public class ConsoleTest {
 
     @Test
     public void test1() {
-        Console console = new Console();
-        console.println(ConsoleColor.GREEN.build("hello word"));
+
 
         System.out.println("\033[36m" + "你好");
         System.out.println("\033[36m" + "你好");
---
+++ src/test/java/com/javacv/test/TessTest.java
@@ -0,0 +1,129 @@
+package com.javacv.test;
+
+import org.bytedeco.javacpp.BytePointer;
+import org.bytedeco.leptonica.BOX;
+import org.bytedeco.leptonica.BOXA;
+import org.bytedeco.leptonica.PIX;
+import org.bytedeco.tesseract.ETEXT_DESC;
+import org.bytedeco.tesseract.ResultIterator;
+import org.bytedeco.tesseract.TessBaseAPI;
+import org.junit.Test;
+
+import static org.bytedeco.leptonica.global.lept.pixDestroy;
+import static org.bytedeco.leptonica.global.lept.pixRead;
+import static org.bytedeco.tesseract.global.tesseract.RIL_TEXTLINE;
+import static org.bytedeco.tesseract.global.tesseract.RIL_WORD;
+import static org.bytedeco.tesseract.global.tesseract.TessMonitorCreate;
+
+public class TessTest {
+
+    @Test
+    public void test1() {
+
+
+        BytePointer outText;
+
+        TessBaseAPI api = new TessBaseAPI();
+        // Initialize tesseract-ocr with English, without specifying tessdata path
+        if (api.Init("/Users/jiangfei/IdeaProjects/opencv/tesseract/tessdata", "chi_sim") != 0) {
+            System.err.println("Could not initialize tesseract.");
+            System.exit(1);
+        }
+
+        // Open input image with leptonica library
+        PIX image = pixRead("/Users/jiangfei/IdeaProjects/opencv/tesseract/22.jpeg");
+        api.SetImage(image);
+        // Get OCR result
+        outText = api.GetUTF8Text();
+        System.out.println("OCR output:\n" + outText.getString());
+
+        // Destroy used object and release memory
+        api.End();
+        outText.deallocate();
+        pixDestroy(image);
+    }
+
+    /**
+     * result iterator
+     */
+    @Test
+    public void test2(){
+
+        BytePointer outText;
+
+        TessBaseAPI api = new TessBaseAPI();
+        // Initialize tesseract-ocr with English, without specifying tessdata path
+        if (api.Init("/Users/jiangfei/IdeaProjects/opencv/tesseract/tessdata", "chi_sim_vert") != 0) {
+            System.err.println("Could not initialize tesseract.");
+            System.exit(1);
+        }
+
+        // Open input image with leptonica library
+        PIX image = pixRead("/Users/jiangfei/IdeaProjects/opencv/tesseract/22.jpeg");
+        api.SetImage(image);
+
+        ETEXT_DESC recoc = TessMonitorCreate();
+        api.Recognize(recoc);
+
+        ResultIterator ri = api.GetIterator();
+        int pageIteratorLevel = RIL_WORD;
+        if (ri != null) {
+            do {
+                outText = ri.GetUTF8Text(pageIteratorLevel);
+                float conf = ri.Confidence(pageIteratorLevel);
+                int[] x1 = new int[1], y1 = new int[1], x2 = new int[1], y2 = new int[1];
+                ri.BoundingBox(pageIteratorLevel, x1, y1, x2, y2);
+                String riInformation = String.format("word: '%s';  \tconf: %.2f; BoundingBox: %d,%d,%d,%d;\n", outText.getString(), conf, x1[0], y1[0], x2[0], y2[0]);
+                System.out.println(riInformation);
+
+                outText.deallocate();
+            } while (ri.Next(pageIteratorLevel));
+        }
+
+        // Destroy used object and release memory
+        api.End();
+        pixDestroy(image);
+    }
+
+    @Test
+    public void test3(){
+
+        BytePointer outText;
+
+        TessBaseAPI api = new TessBaseAPI();
+        // Initialize tesseract-ocr with English, without specifying tessdata path
+        if (api.Init("/Users/jiangfei/IdeaProjects/opencv/tesseract/tessdata", "chi_sim") != 0) {
+            System.err.println("Could not initialize tesseract.");
+            System.exit(1);
+        }
+
+        // Open input image with leptonica library
+        PIX image = pixRead("/Users/jiangfei/IdeaProjects/opencv/tesseract/11.png");
+        api.SetImage(image);
+
+
+        // Lookup all component images
+        int[] blockIds = {};
+        BOXA boxes = api.GetComponentImages(RIL_TEXTLINE, true, null, blockIds);
+
+        for (int i = 0; i < boxes.n(); i++) {
+            // For each image box, OCR within its area
+            BOX box = boxes.box(i);
+            api.SetRectangle(box.x(), box.y(), box.w(), box.h());
+            outText = api.GetUTF8Text();
+            String ocrResult = outText.getString();
+            int conf = api.MeanTextConf();
+
+            String boxInformation = String.format("Box[%d]: x=%d, y=%d, w=%d, h=%d, confidence: %d, text: %s", i, box.x(), box.y(), box.w(), box.h(), conf, ocrResult);
+            System.out.println(boxInformation);
+
+            outText.deallocate();
+        }
+
+        // Destroy used object and release memory
+        api.End();
+        pixDestroy(image);
+
+    }
+
+}
